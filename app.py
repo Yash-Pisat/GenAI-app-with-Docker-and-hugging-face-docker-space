7375dae7e46362107ff1e4ed8d59428c4c295f15
Yash Uday Pisat <yashpisat@gmail.com>
2024-09-06 00:21:46 +0200

    Changes to docker file

--- app.py
+++ app.py
@@ -18,4 +18,8 @@ def home():
 
 @app.get("/generate")
 def generate(text:str):
-    
+    ## use the pipeline to generate text from given input text
+    output = pipe(text)
+
+    ## return the generated text in json response
+    return {"output":output[0]['Generated_text']}
